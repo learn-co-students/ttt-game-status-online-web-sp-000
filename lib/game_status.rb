9bad1c50008c7c707f7c1eee4ba4fda12818d8b0
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-21 15:11:23 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,52 @@
 # Helper Method
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+  !(board[index].nil? || board[index] == "   " || board[index] == "  " || board[index] == " " || board[index] == "")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    if position_taken?(board, win_combination[0]) == true
+      if board[win_combination[0]] == board[win_combination[1]] && board[win_combination[0]] == board[win_combination[2]]
+        return win_combination
+      else
+        false
+      end
+    else
+    end
+  end
+  false
+end
+
+def full?(board)
+  board.all? do |position|
+    position == "X" || position == "O"
+  end
+end
+
+def draw?(board)
+  if full?(board) == true && won?(board) == false
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if !(won?(board)) == false || full?(board) == true || draw?(board) == true
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  win = won?(board)
+  if won?(board) == false
+    nil
+  else
+    return board[win[0]]
+  end
+end
