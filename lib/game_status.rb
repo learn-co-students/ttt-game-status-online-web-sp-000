caf33d84533d1bac6a795f917b24d50a2141c638
Aaron Thompson <thompson_ad@outlook.com>
2018-12-16 16:56:21 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,62 +7,64 @@ end
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
-  [0,1,2], [3,4,5], [6,7,8],
-  [0,3,6], [1,4,7], [2,5,8],
-  [0,4,8], [2,4,6]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
 ]
 
-def won?(board)
-  winner = nil
-  WIN_COMBINATIONS.each do |combo|
-    if combo.all? {|idx| board[idx] == "X"}
-      winner = combo
-    elsif combo.all? {|idx| board[idx] == "O"}
-      winner = combo
-    else
-      false
-    end
-  end
-  if winner != nil
-    winner
-  else
-    false
-  end
-end
-
 def full?(board)
-  board.all? {|token| token == "X" || token == "O"}
+  board.none? do |space|
+    space == " "
+  end
 end
 
 def draw?(board)
-
-  if full?(board)
-    if won?(board) == false
-      true
-    else
-      false
-    end
+  if (full?(board) == true) && (won?(board) == false)
+    true
   else
     false
   end
 end
 
-def over?(board)
-  if won?(board) == false && draw?(board) == false
-    false
+def won?(board)
+  winning_combination = nil
+  WIN_COMBINATIONS.each do |combo|
+    if combo.all? {|idx| board[idx] == "X"}
+      winning_combination = combo
+    elsif combo.all? {|idx| board[idx] == "O"}
+      winning_combination = combo
+    else
+      false
+    end
+  end
+  if winning_combination != nil
+    winning_combination
   else
-    true
+    false
   end
 end
 
 def winner(board)
-  win = won?(board)
+  if won?(board) == false
+    nil
+  elsif (won?(board)).all? {|idx| board[idx] == "X"}
+    "X"
+  elsif (won?(board)).all? {|idx| board[idx] == "O"}
+    "O"
+  end
+end
 
-  if win == nil || win == false
-    return nil
-  elsif win.all? {|idx| board[idx] == "X"}
-    return "X"
-  elsif win.all? {|idx| board[idx] == "O"}
-    return "O"
+def over?(board)
+  if won?(board) != false
+    true
+  elsif draw?(board) == true
+    true
+  else
+    false
   end
 end
