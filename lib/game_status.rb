d585ba279561fe7d17959f23582e35d048b6c8b6
Joe Ganobsik <jganobsik@gmail.com>
2019-01-09 00:17:27 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -90,6 +90,25 @@ end
 
     
     
+def turn(board)
+  puts 'Please enter 1-9:'
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+def play(board)
+  turn(board) until over?(board)
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
     
   
     
