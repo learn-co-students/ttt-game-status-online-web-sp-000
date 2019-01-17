6903371f465c9b926c895a1361223987f252c7b8
Lizzie Chan <lchan217@gmail.com>
2019-01-17 22:23:36 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,79 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+
+def won?(board)
+  WIN_COMBINATIONS.each do|combo|
+    
+  win_index_1 = combo[0]
+  win_index_2 = combo[1]
+  win_index_3 = combo[2]
+  
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+  
+  if position_1 == "X" &&
+    position_2 == "X" &&
+    position_3 == "X"
+      return combo
+  elsif position_1 == "O" &&
+    position_2 == "O" &&
+    position_3 == "O"
+      return combo
+  else
+  end
+end
+false
+end
+
+def full?(board)
+  if board.all? {|word| word == "X" || word =="O"}
+    true 
+  else
+    false
+  end
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    true
+  else
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board) == true
+    return true
+  else
+  end
+end
+
+def winner(board)
+  WIN_COMBINATIONS.each do|combo|
+   win_index_1 = combo[0]
+  win_index_2 = combo[1]
+  win_index_3 = combo[2]
+  
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return "X"
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return "O"
+    end
+  end
+  nil
+end
