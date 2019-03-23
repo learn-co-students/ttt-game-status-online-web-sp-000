926166a7674bae2f7219555104a3816402904328
carefree-closure-1550 <leo.kalshteyn@gmail.com>
2019-03-23 22:06:01 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,44 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [0,4,8],
+  [2,4,6],
+  [2,5,8],
+]
+
+
+def won?(board)
+  WIN_COMBINATIONS.find do |combo|
+    combo.all?{|index| board[index] == "X"} || combo.all?{|index| board[index] == "O"}
+  end
+end
+
+def full?(board)
+  board.all?{|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    true
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  end
+end
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  else
+    return nil
+  end
+end
