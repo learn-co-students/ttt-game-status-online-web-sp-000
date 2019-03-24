59ffedc828f2666e08c7228046d0f6de1eef0e24
mirthful-flag-5763 <irinagamb@gmail.com>
2019-03-24 23:42:14 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,51 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+
+def won?(board)
+
+WIN_COMBINATIONS.each_index do |i|
+  combOne = WIN_COMBINATIONS[i]
+
+  if (board[combOne[0]] == 'X' &&
+     board[combOne[1]] == 'X'  &&
+     board[combOne[2]] == 'X') ||
+     (board[combOne[0]] == 'O' &&
+     board[combOne[1]] == 'O'  &&
+     board[combOne[2]] == 'O')
+     return combOne
+  end
+end
+  return false
+end
+
+def full?(board)
+   board.each do |item|
+     if item == " "
+       return false
+     end
+   end
+
+   return true
+end
+
+
+def draw?(board)
+   full?(board) && !won?(board)
+end
+
+
+def over?(board)
+  won?(board) || full?(board) || draw?(board)
+end
+
+
+#condtion ? conditon is true : condtion is false 
+
+def winner(board)
+  w = won?(board)
+  w != false ? board[w[0]] : nil
+end
