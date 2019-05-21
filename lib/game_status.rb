a2ac377d6e55dc693d596cd3b1ce5e4b87d7151a
Connor Cowan <ccowan003@comcast.net>
2019-05-21 18:55:13 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,22 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+#board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [6,4,2],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+]
+
+def won?(board)     #check the board return true if win, false if not
+  
+  board.each do |win|
+    if win == WIN_COMBINATIONS
+      winning = true
+end
