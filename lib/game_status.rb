7cd08dcdb35e4c65988d0bdd98926fdeed6f591a
Connor Cowan <ccowan003@comcast.net>
2019-05-21 19:42:27 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,8 +3,7 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
-#board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+# Defined your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
   [0,1,2],
@@ -18,8 +17,31 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)     #check the board return true if win, false if not
-  
-  board.each do |win|
-    if win == WIN_COMBINATIONS
-      winning = true
+
+  WIN_COMBINATIONS.detect do |combo|          #iterating (detecting) CONSTANT for winning combos
+    board[combo[0]] == board[combo[1]] &&     #conditions for a WIN
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])           #takes board and index as arguments, returns true if position empty
+  end
+end
+
+def full?(board)          #accepts board, returns true if every element in the board contains either an "X" or an "O"
+  board.all? do |token|   #iterator: all? block passed to it must return true for every iteration
+    token == "X" ||       #true for only X and O's
+    token == "O"
+  end
+end
+
+def draw?(board)
+  !won?(board) && full?(board)      #call to methods if board NOT won, but is FULL! Returns true only in those cases!
+end
+
+def over?(board)
+    won?(board) || draw?(board) || full?(board)
+end
+
+def winner?(board)
+  if winning_combo == win?(board)
+    board[winning_combo.first]
+  end
 end
