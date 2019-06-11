202cc65ed85fcb95b9313f4bcecdb0103d326f53
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-11 17:46:03 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,6 +6,10 @@ end
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 def won?(board)
-  board[WIN_COMBINATIONS[0][0]] == "X" && board[WIN_COMBINATIONS[0][1]] == "X" && board[WIN_COMBINATIONS[0][2]] == "X"
-   WIN_COMBINATIONS[0]
+  board[WIN_COMBINATIONS[0].each {|player| if player == "X"}] 
+end
+
+
+board[WIN_COMBINATIONS[0][0]] == "X" && board[WIN_COMBINATIONS[0][1]] == "X" && board[WIN_COMBINATIONS[0][2]] == "X"
+ WIN_COMBINATIONS[0]
 end
