a5256fae300b6a59249b9b6621cc5728ed104e58
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-11 18:27:49 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,4 +1,4 @@
-# Helper Method
+require 'pry'
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
@@ -6,10 +6,13 @@ end
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 def won?(board)
-  board[WIN_COMBINATIONS[0].each {|player| if player == "X"}] 
+  # binding.pry
+  WIN_COMBINATIONS.detect do |combo|
+    if board[combo[0]] == board[combo[1]] && board[[1]] == board[combo[2]]
 end
-
-
-board[WIN_COMBINATIONS[0][0]] == "X" && board[WIN_COMBINATIONS[0][1]] == "X" && board[WIN_COMBINATIONS[0][2]] == "X"
- WIN_COMBINATIONS[0]
 end
+end
+# board[WIN_COMBINATIONS[0][0]] == "X" && board[WIN_COMBINATIONS[0][1]] == "X" && board[WIN_COMBINATIONS[0][2]] == "X"
+#  WIN_COMBINATIONS[0]
+# end
+# [1,2,3].detect{|i| i.odd?} #=> 1
