edcbdae842b73cdf2dcca36d9e449cb28d99e85a
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-11 22:52:45 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,11 +6,9 @@ end
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 def won?(board)
- if position_taken?(board, index)
     WIN_COMBINATIONS.detect do |combo|
-    board[combo[0]] == board[combo[1]] && board[combo[1]]  == board[combo[2]]
+      position_taken?(board, combo[0]) && board[combo[0]] == board[combo[1]] && board[combo[1]]  == board[combo[2]]
     end
-  end
 end
 
 
