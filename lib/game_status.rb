c24bb66ad15848626c741ba824f15341a30d6e7f
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-11 22:17:29 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,15 +6,16 @@ end
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 def won?(board)
-  # binding.pry
-  WIN_COMBINATIONS.detect do |combo|
-    if board[combo[0]] == [combo[0]]
+ if position_taken?(board, index)
+    WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] && board[combo[1]]  == board[combo[2]]
+    end
+  end
+end
 
-      # && board[[1]] == board[combo[2]]
-      # board[combo]
-end
-end
-end
+
+      # board[combo]end
+
 # board[WIN_COMBINATIONS[0][0]] == "X" && board[WIN_COMBINATIONS[0][1]] == "X" && board[WIN_COMBINATIONS[0][2]] == "X"
 #  WIN_COMBINATIONS[0]
 # end
