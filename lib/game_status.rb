3ec442de89cbdc493e9352a826e4470e00a51197
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-07-31 17:44:31 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,70 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]]
+end
+  
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    #check for player 1 win
+    if check_win_combination?(board, 'X', win_combo)
+      return win_combo
+      #check for player 2 win
+    elsif check_win_combination?(board, 'O', win_combo)
+      return win_combo
+    else
+      return false
+    end
+  end
+end
+
+def check_win_combination?(board, player, win_combo)
+  win_combo.all? do |position|
+    board[position] == player
+  end
+end
+
+def full?(board)
+  if board.include?(' ') || board.include?('')
+    return false
+else
+    return true
+  end
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  end
+end
+
+def over?(board)
+  puts 'is it over?'
+  if won?(board) || draw?(board) || full?(board)
+    return true
+  else
+    puts 'no keep going'
+    return false
+  end
+end
+
+def winner(board)
+  if !won?(board)
+    return nil
+  else WIN_COMBINATIONS.each do |win_combo|
+    if check_win_combination?(board, 'X', win_combo)
+      return 'X'
+    elsif check_win_combination?(board, 'O', win_combo)
+      return 'O'
+    end
+  end
+end
+end
