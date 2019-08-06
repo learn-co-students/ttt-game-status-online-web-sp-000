c44efdeca8b8608713c6c841211115ae5d193c99
Panela Leung <leungpanela@gmail.com>
2019-08-06 14:53:46 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,48 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+
+  [0,4,8],
+  [2,4,6],
+
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
+
+def won?(board)
+WIN_COMBINATIONS.each do |win|
+# code to show all steps
+  #win_pos_1 = win[0]
+  #win_pos_2 = win[1]
+  #win_pos_3 = win[2]
+  #see_pos_1= board[win_pos_1]
+  #see_pos_2= board[win_pos_2]
+  #see_pos_3= board[win_pos_3]
+# code to show streamlined
+  see_pos_1= board[win[0]]
+  see_pos_2= board[win[1]]
+  see_pos_3= board[win[2]]
+
+  if (see_pos_1 == "X" && see_pos_2 == "X" && see_pos_3 == "X") || (see_pos_1 == "O" && see_pos_2 == "O" && see_pos_3 == "O")
+    return win
+  end
+end
+ false
+end
+
+def full?(board)
+  board.each do |space|
+    check_space= space
+  if space != " "
+    return true
+  else
+    return false
+  end
+end
+end
