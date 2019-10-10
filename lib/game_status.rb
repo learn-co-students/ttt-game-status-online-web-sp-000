e8d8ef20909fdb8241f026c2be9d6e13b7fac74e
Paul Marshall <pmarshal33@gmail.com>
2019-10-10 02:01:12 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,66 @@
 # Helper Method
+require 'pry'
+
 def position_taken?(board, index)
+  binding.pry
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [ 
+  [0,1,2],[3,4,5],[6,7,8],
+  [0,3,6],[1,4,7],[2,5,8],
+  [0,4,8],[2,4,6],
+]
+def won?(board)
+      #binding.pry 
+  if board == [" "," "," "," "," "," "," "," "," "]
+    return false
+  end
+      
+#Winning Combinations
+    WIN_COMBINATIONS.detect do |win_combo|
+      board[win_combo[0]] == board[win_combo[1]] && board[win_combo[0]] == board[win_combo[2]] && board[win_combo[0]] != " " 
+    end
+end
+
+def full?(board)
+        if board == ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+          return true
+      else return false
+    end
+end
+
+def draw?(board)
+     if full?(board) && !won?(board)
+       true
+         else
+           false
+     end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+        true
+    else 
+        false
+  end
+end
+
+def winner(board)
+ # binding.pry
+    if won?(board)
+      board[won?(board).first] 
+    end
+end
+  
+  
+  
+  
+
+
+
+
+
+
+
+
