c40d05a4d0e05bb36c4f040089945af810c05a17
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-16 06:58:33 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,32 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+def WIN_COMBINATIONS
+  [0,1,2],
+  [0,3,6],
+  [0,4,8],
+  [1,4,7],
+  [2,5,8],
+  [2,4,6]
+  
+
+
+def won?
+
+end
+
+
+
+
+def full?
+end
+
+def draw?
+end
+
+def over?
+end
+
+
+def winner
+end
