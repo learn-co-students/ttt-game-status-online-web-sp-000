15674870d77c47f2f071a893186ce34513152e40
Alethea Cary <aletheacary@gmail.com>
2019-11-13 15:27:39 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,20 @@
 # Helper Method
+require 'pry'
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
+end
+
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
+   [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+   
+def won?(board)
+  WIN_COMBINATIONS.each do |indexes|
+    if indexes.all?{|index| index == "X"} || indexes.all?{|index| index == "0"}
+      indexes
+    else
+      FALSE
+    end
+  end
 end
 
 # Define your WIN_COMBINATIONS constant
