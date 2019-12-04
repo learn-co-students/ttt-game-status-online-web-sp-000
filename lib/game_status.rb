e800c298a1009903e8e2b589fb739347f203a29f
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2019-12-04 15:53:08 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,7 +1,7 @@
-# Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
-end
+end# Helper Method
+
 
 WIN_COMBINATIONS = [
   [0, 1, 2],
@@ -14,21 +14,56 @@ WIN_COMBINATIONS = [
   [2, 4, 6]
 ] # Define your WIN_COMBINATIONS constant
 def won?(board)
-for each win_combination in WIN_COMBINATIONS
-  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-  # grab each index from the win_combination that composes a win.
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
+  win_combo = nil
+  WIN_COMBINATIONS.each do |win_combination|
+    # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+    # grab each index from the win_combination that composes a win.
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
-  position_1 = board[win_index_1] # load the value of the board at win_index_1
-  position_2 = board[win_index_2] # load the value of the board at win_index_2
-  position_3 = board[win_index_3] # load the value of the board at win_index_3
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
 
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination # return the win_combination indexes that won.
-  else
-    false
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      win_combo = win_combination # return the win_combination indexes that won
+      end
+    if position_1 == "O" && position_2 == "O" && position_3 == "O"
+       win_combo = win_combination # return the win_combination indexes that won.
+     end
+  end
+  return win_combo
+end
+
+def full?(board)
+  turn_count(board) == 9
+end
+
+def full?(board)
+  board.none? do |i|
+    i == " " || i.nil?
   end
 end
+
+def draw?(board)
+  full?(board) && !won?(board)
 end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
+
+def winner(board)
+  if win_combo = won?(board)
+    board[win_combo]
+  end
+end
+
+def winner(board)
+  if won?(board) != nil
+    winner = board[won?(board)[0]]
+  end
+end
+
+board = Array.new(9, " ")
