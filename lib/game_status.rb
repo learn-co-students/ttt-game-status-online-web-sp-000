c5ab9f8bc24065798f7f0e1e22871d836a960f1a
Andrew Bourgeois <andrew@olympicmedia.com>
2020-03-11 23:59:16 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,36 @@
+require "pry"
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [0,4,8], # Left-to-right diagonal
+  [2,4,6]  # Right-to-left diagonal
+]
+
+
+# The #won? method that returns 'true' if there is a win and 'false not'
+def won?(board)
+  WIN_COMBINATIONS.select do |win_combination|
+  
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+  
+  binding.pry
+  position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+  end
+end
