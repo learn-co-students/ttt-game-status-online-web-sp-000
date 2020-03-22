20487839b371eb581fe9ab507640bca8a81adb89
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-03-22 01:11:12 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,47 @@
 # Helper Method
+require 'pry'
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [6,4,2],
+  [0,3,6],
+  [2,5,8],
+  [1,4,7]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] && 
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+  board.all?{|pipes| pipes == "X" || pipes == "O"}
+end
+
+def draw?(board)
+  if won?(board) == false && full?(board) == true 
+    return true
+  elsif won?(board) == true 
+    return false
+  end
+end
+
+  
+  
+#def over?
+
+
+#def winner(board)
+  #return "X" or "O" given the game is a winning board
+#end
