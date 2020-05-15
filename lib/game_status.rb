e8713ac2fb77b1b0e9cd52ac6e8f6155838e1aec
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-15 03:41:05 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,60 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    if position_taken?(board, combo[0]) &&
+        board[combo[0]] == board[combo[1]] &&
+        board[combo[1]] == board[combo[2]]
+        return combo
+    end
+  end
+  return nil
+end
+
+def full?(board)
+  count = board.select do |space|
+    space == "X" || space == "O"
+  end
+    count.length() == 9
+end
+
+def draw?(board)
+  if full?(board) == true && won?(board) == nil
+    return true
+  elsif full?(board) == false && won?(board) == nil
+    return false
+  elsif won?(board) != nil
+    return false
+  end
+end
+
+def over?(board)
+  full?(board) == true || draw?(board) == true || won?(board) != nil
+end
+
+def winner(board)
+  if won?(board) != nil
+    winning_combo = won?(board)
+    return board[winning_combo[0]]
+  else
+    return nil
+  end
+end
+
+# board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+# # full?(board)
+# print won?(board)
+# print winner(board)
+# # draw?(board)
