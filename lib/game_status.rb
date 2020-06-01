aa9fed67b80753a7f355f76a25d11b4192b43e04
Cody Alvarez <cody_alvarez19@outlook.com>
2020-06-01 02:52:14 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
@@ -15,20 +16,8 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  board.each do |win_combination| WIN_COMBINATIONS
+  WIN_COMBINATIONS.detect do |combo|
+    position_taken?(combo[0]) && board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]]
 
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
-    else
-    false
-    end
   end
 end
