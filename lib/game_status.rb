6458462866461d5493d265c9be72349ffc17c464
Chris Baum <christopher.baum@valpo.edu>
2021-01-05 03:51:40 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,42 +15,62 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
+
 def won?(board)
-  winning_combo = [""]
-  (WIN_COMBINATIONS.count).times{ |i|
-        (WIN_COMBINATIONS[i].all?{|j| (board[j] == "X") || (board[j] == "O")}) == true ? (winning_combo = WIN_COMBINATIONS[i]) : nil
-      }
-  winning_combo
+  num_winning_combos = WIN_COMBINATIONS.count #determines count of the number of winning combinations
+
+# Look at the board for "X" and "O" in the winning combination indexes
+  (num_winning_combos).times {|i|
+      win_combo = [""]
+
+      # if all values equal X return true
+      all_X = WIN_COMBINATIONS[i].all? do |j|
+        board[j] == "X"
+      end
+
+      # if all values euql O return true
+      all_O = WIN_COMBINATIONS[i].all? do |n|
+        board[n] == "O"
+      end
+
+      # if all_X or all_O true then return winning combination
+      if (all_X == true || all_O == true)
+        status = WIN_COMBINATIONS[i]
+        #puts "#{status}"
+        return status
+      end
+
+  }
+false #returns false if no winning combination found
 end
 
 def full?(board)
- board.detect{|i| i == " "} ? false : true
+ board.detect{|i| (i == " ") || (i == "")} ? false : true #If no blank spaces are found return true
 end
 
 def draw?(board)
-  winning_combo == [""] ? false : true
+  if (won?(board) == false && full?(board) == true) #if no winning combination found and the board is full, return true
+    true
+  else
+    false
+  end
+
 end
 
 def over?(board)
-
+  if (draw?(board) == true || won?(board) != false) #if there is a draw or a winner then return true
+    true
+  else
+    false
+  end
 end
+
 
 def winner(board)
-
+  who_winner = won?(board) #determines which rows the board was won in
+  if who_winner != false
+    board[who_winner[0]] #searches the 1st value of the winning index to see who the winner was
+  else
+    nil
+  end
 end
-
-#  if (board.any?{|i| i == "X" || i == "O"}) == true  #checks if board is empty
-#    (WIN_COMBINATIONS.count).times{ |i|
-#      (WIN_COMBINATIONS[i].all?{|j| board[j] == "X"}) == true ? WIN_COMBINATIONS[i] : nil
-#    }
-#  else
-#    false
-#  end
-
-#won?(board)
-
-#def won?(board)
-  #board.any?{|i| i == "X" || i == "O"}
-  #board.each_index.select{|i| board[i] == "X"}
-  #puts "#{WIN_COMBINATIONS[1].all?{|j| board[j] == "X"}}"
-#end
