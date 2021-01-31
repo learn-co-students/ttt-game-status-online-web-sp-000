0029a961116e47e2892397b9a1a8f54b1d79de1d
Ruthie C <lilyruthcole@gmail.com>
2021-01-31 22:28:34 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,81 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+  ]
+
+def won?(board) 
+  result = ''
+  
+  if board ==   [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    return FALSE
+  end 
+
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+     result = win_combination 
+  
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+     result = win_combination
+    
+   end 
+end 
+if result != '' 
+  return result 
+else return false 
+end 
+end 
+
+
+
+def full?(board)
+  board.all? {|space| space == "X" || space == "O"}
+end
+
+
+def draw?(board)
+  if won?(board) == FALSE && full?(board) == TRUE 
+    return TRUE 
+  else return FALSE 
+  end 
+end 
+
+def over?(board) 
+ if full?(board) == TRUE 
+   return TRUE 
+ elsif won?(board) != FALSE
+   return TRUE 
+ else return FALSE 
+ end 
+end 
+
+def winner(board) 
+  
+  if over?(board) == FALSE 
+  return nil 
+  elsif draw?(board) == TRUE 
+  return nil 
+  else  
+     winner_array = won?(board)
+     winner_index = winner_array[0]
+     winner = board[winner_index]
+     return winner 
+  end 
+end 
