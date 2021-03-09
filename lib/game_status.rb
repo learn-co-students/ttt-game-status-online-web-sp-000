603e12b66f528d719599154034ad602fc97b4145
Lj <tnedutsl457892@gmail.com>
2021-03-09 23:55:58 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-def WIN_COMBINATIONS = [
+WIN_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -14,4 +14,3 @@ def WIN_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6]
 ]
-end
