8e023f0489ec53d50921ac4972c74143ff310049
Lj <tnedutsl457892@gmail.com>
2021-03-10 23:41:43 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,9 +24,10 @@ def won(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-  if position_1 == "X" && position_2 == "X" && position_3 == "X"
-    return win_combination
-  else
-    false
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    else
+    end
   end
+  return nil
 end
