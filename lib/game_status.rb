ce2ea2234e4090fe50725c8adac8462edf8854cf
Lj <tnedutsl457892@gmail.com>
2021-03-10 23:35:23 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,8 +15,8 @@ WIN_COMBINATIONS = [
   [2, 4, 6]
 ]
 
-def won
-  for each win_combination in WIN_COMBINATIONS
+def won(board)
+  for each WIN_COMBINATIONS in win_combination
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
