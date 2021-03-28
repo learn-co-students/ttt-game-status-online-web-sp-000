8d9e759fd6e365a9d3d352e4c3adebfc272f77bf
Lj <tnedutsl457892@gmail.com>
2021-03-28 01:30:48 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -63,3 +63,23 @@ def over?(array)
     return false
   end
 end
+
+def winner(array)
+  if won?(array)
+    WIN_COMBINATIONS.each do |win_combination|
+      position_1 = array[win_combination[0]]
+      position_2 = array[win_combination[1]]
+      position_3 = array[win_combination[2]]
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+        return "X"
+      elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        return "O"
+      else
+        # do nothing
+      end
+    end
+  else
+    return nil
+  end
+end
+  
