0514a75b63c3f3cfc198c0e23a38e1b845bd2535
powell956 <powell956@gmail.com>
2021-03-30 19:24:25 -0400

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,66 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    #store values from board from the win combination being checked this iteration
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    #check the values at the win combination indexes for a same-token streak
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    end
+  end
+  false
+end
+
+def full?(board)
+  board.none? do |space|
+    space == " "
+  end
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    true
+  end
+end
+
+def winner(board)
+  #store array of winning indices
+  win_combination = won?(board)
+  #reference the token at an arbitrary winning index, which should be the token of the winning player
+  if won?(board)
+    #reference the token at an arbitrary winning index, which should be the token of the winning player
+    board[win_combination[0]]
+  else
+    nil
+  end
+end
