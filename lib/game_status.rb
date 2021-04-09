d8293573597f6f47b706786aa5c1251cf97c4941
Drew Keat <thedrewkeat@gmail.com>
2021-04-09 13:47:08 -0600

    #won?(board) works!

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,19 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
+#Should accept a board as an argument and return false/nil if there is no win combination present in the board and return the winning
+# combination indexes as an array if there is a win.
+def won?(board)
+  WIN_COMBINATIONS.each do |combo|
+    check1 = board[combo[0]]
+    check2 = board[combo[1]]
+    check3 = board[combo[2]]
+    final_check = [check1,check2,check3]
+    if final_check.all?{|char| char == check1} && check1 != " "
+      return combo
+    end
+  end
+  return false
+end
+
