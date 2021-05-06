9b30b9a95e507c9b511fa1b1b0b8d6c321341e0e
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-06 16:26:30 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -62,17 +62,21 @@ WIN_COMBINATIONS =[[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8]
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    #win_combination.each do |win_index|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
+    win_combination.each do |win_index|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
       
-    if board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X"
-      return win_combination
-    elsif board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O"
-      return win_combination
-    else
-      return false
+      if board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X"
+        return win_combination
+      
+      elsif board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O"
+        return win_combination
+      
+      else
+        return false
+      
+      end
     end
   end
 end
