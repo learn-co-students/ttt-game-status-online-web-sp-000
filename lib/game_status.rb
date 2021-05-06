5bb499cdda423ce4f71af1bb96b78ea05e62725c
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-06 17:10:41 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -72,15 +72,12 @@ def won?(board)
     elsif board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O"
       return win_combination
     
-    elsif board[win_index_1] != "X" && board[win_index_2] != "X" && board[win_index_3] != "X"
-      return false
-      
-    else board[win_index_1] != "O" && board[win_index_2] != "O" && board[win_index_3] != "O"
+    else 
       return false
     
     end
   end
-  
+  win_combination
 end
 
 
