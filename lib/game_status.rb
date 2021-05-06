e9f72164bfe3b426dcacbd9a53efcff4b2d8268b
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-06 16:23:23 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,3 +1,57 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def play(board)
+  counter=0
+  until counter==9
+    turn(board)
+    counter += 1
+  end
+end
+
+def turn_count(board) 
+  count =0
+  board.each do |occupied|
+    if occupied == "X" || occupied == "O"
+    count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
@@ -8,22 +62,22 @@ WIN_COMBINATIONS =[[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8]
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    #try nested array here
-    win_combination.each do |win_index|
-      position_taken?(board, win_index)
-      won?(board)
-      #we just want the winning array to print or false/nil to print if not winning
-      #maybe put win_index array into position_taken and run true false
-      #win_index.each do |position|
-
-   # position_1 = board[win_index_1]
-   # position_2 = board[win_index_2]
-   # position_3 = board[win_index_3]
-    
-    #if position_1 ==  position_2 && position_2 == position_3 && position_taken#?(board, win_index_1)
-      #return win_combination
-    #else 
-      #false
+    #win_combination.each do |win_index|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+      
+    if board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X"
+      return win_combination
+    elsif board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O"
+      return win_combination
+    else
+      return false
     end
   end
 end
+
+
+
+
+  
